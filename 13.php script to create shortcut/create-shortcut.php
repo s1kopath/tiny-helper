efa46a2ea554a2f56b8d74654e002e9fd9adb0d6
Md. Asaduzzaman <asaduzzaman.arob@gmail.com>
2025-04-15 13:42:12 +0600

    Update create-shortcut.php

--- 13.php script to create shortcut/create-shortcut.php
+++ 13.php script to create shortcut/create-shortcut.php
@@ -1,6 +1,6 @@
 <?php
 $targetFolder = __DIR__ . '/core/storage/app/public'; // The folder you want to link to
-storage/app/public/media
+
 $linkFolder = __DIR__ . '/storage'; // The shortcut link you want to create
 
 // Check if the link already exists
