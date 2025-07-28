d45fcb2fd74bc7263dc4bcc1ff7323ba45051bae
Md. Asaduzzaman <asaduzzaman.arob@gmail.com>
2025-07-28 11:32:16 +0600

    Update firebase-messaging-sw.js

--- 23.laravel-fcm-push/firebase-messaging-sw.js
+++ 23.laravel-fcm-push/firebase-messaging-sw.js
@@ -16,6 +16,7 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const notificationTitle = payload.notification.title;
+    // const notificationTitle = payload.data.title;
     const notificationOptions = {
         body: payload.notification.body,
         icon: '/assets/images/logo/logo-icon.png' // optional    
